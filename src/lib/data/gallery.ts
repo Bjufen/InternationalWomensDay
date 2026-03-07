ef72d647a6d49bd0801aa0368756eeac09ec08db
Yusuf Shehadeh <y.shehadeh@knuddels.de>
2026-03-07 04:53:36 +0100

    Add the photos and their descriptions

--- src/lib/data/gallery.ts
+++ src/lib/data/gallery.ts
@@ -9,103 +9,105 @@ export const photos: GalleryPhoto[] = [
 	{
 		src: 'photos/01.jpg',
 		alt: 'Foto 1',
-		title: 'Ein besonderer Moment',
-		description: 'Beschreibung kommt hier hin.'
+		title: 'The Subject of Today\'s Exhibition',
+		description: 'I mean just look at that smile. IS IT NOT TO DIEEEEE FOR???? Anyone who says no: STRAIGHT TO JAIL!'
 	},
 	{
 		src: 'photos/02.jpg',
 		alt: 'Foto 2',
-		title: 'Zusammen unterwegs',
-		description: 'Beschreibung kommt hier hin.'
+		title: 'Pure Euphorie',
+		description: 'Tatjana kann sich über die randomste Sachen einfach freuen. I mean just look at those eyes. Habt ihr jemals jemand gesagt, die so high on life war?'
 	},
 	{
 		src: 'photos/03.jpg',
 		alt: 'Foto 3',
-		title: 'Dein Lächeln',
-		description: 'Beschreibung kommt hier hin.'
+		title: 'ALARMSTUFE ROT',
+		description: 'Tatjana vergisst es manchmal zu essen. Und auf einmal siehst du so ein Gesicht. Wenn du jemals dieses Gesicht siehst: FEED TATJANA IMMEDIATELY'
 	},
 	{
 		src: 'photos/04.jpg',
 		alt: 'Foto 4',
-		title: 'Unvergesslich',
-		description: 'Beschreibung kommt hier hin.'
+		title: 'Resurrection',
+		description: 'Tatjana hat nun was zu essen bekommen. Sie ist jetzt für die nächste Zeit wieder in Bestform!'
 	},
 	{
 		src: 'photos/05.jpg',
 		alt: 'Foto 5',
-		title: 'Abenteuer',
-		description: 'Beschreibung kommt hier hin.'
+		title: 'Das Auge der Künstlerin',
+		description: 'Tatjana liebt es Momente festzuhalten (was sie auch richtig gut macht). Wenn du Glück hast, dann kann es sein, dass du eines Tages einer solcher Fotos mal geschickt bekommst.'
 	},
 	{
 		src: 'photos/06.jpg',
 		alt: 'Foto 6',
-		title: 'Gemeinsame Zeit',
-		description: 'Beschreibung kommt hier hin.'
+		title: 'Photogenic',
+		description: 'WHOOO BARNEY STINSON????? Hier siehst du die wahre Queen. Egal ob sie bereit ist oder nicht, es ist immer ein Top Foto wenn Tatjana da drin ist.'
 	},
 	{
 		src: 'photos/07.jpg',
 		alt: 'Foto 7',
-		title: 'Von Herzen',
-		description: 'Beschreibung kommt hier hin.'
+		title: 'Fenster zur Seele',
+		description: 'Dieses Foto nenne ich LOOOK AT THOSE TEEEETH. Das hat ein simpler Grund. DIE SIND SOOOOO CUTEEEEEEEEE IC OULD DIEEE.\n' +
+			'Aber das ist nicht alles an dieses Foto: Frag mich nicht warum, aber HIER SIEHT MAN SO EIN GLÄNZEN in ihre Augen. Manche sagen \'Lichtreflexion\', ich sage: Das ist ihre Seele, die durchscheint. Wenn sie so glänzt, ist sie mit 100% Authentizität dabei.'
 	},
 	{
 		src: 'photos/08.jpg',
 		alt: 'Foto 8',
-		title: 'Für immer',
-		description: 'Beschreibung kommt hier hin.'
+		title: 'CAMERA OVERHEATING',
+		description: 'Ein Blick wie Feuer. Aber warte mal, du kriegst nicht nur die Ehre dieses Blick zu sehen. DU KRIEGST AUCH EINE KIPPEEEEEEEEEEEEEEE. Tatjana dreht die besten Kippen der Welt, mit Liebe und Präzision.'
 	},
 	{
 		src: 'photos/09.jpg',
 		alt: 'Foto 9',
-		title: 'Glücksmoment',
-		description: 'Beschreibung kommt hier hin.'
+		title: 'The Hustle is Real',
+		description: 'Ist es überhaupt fair? Ne. Man fragt sich ehrlich manchmal wie sie sich überhaupt konzentrieren kann mit so einem Gesicht. Ein Grund: TATJANA IS A FUCKING HUSTLER. Sie reist ganz über Deutschland (and beyond) und klärt alles. A fire to distinguish in Dortmund? No problem for Tatjana. An installation in Munich? Child\'s playyyyyyy. Whatever you need, if you send Tatjana, forget about because: It\'s already doneeeeeeeee.'
 	},
 	{
 		src: 'photos/10.jpg',
 		alt: 'Foto 10',
-		title: 'Unser Weg',
-		description: 'Beschreibung kommt hier hin.'
+		title: 'OH SHIT WE FORGOT TO FEED TATJAAAAAAANA',
+		description: 'Priorität Nr. 1: ESSEN!!!'
 	},
 	{
 		src: 'photos/11.jpg',
 		alt: 'Foto 11',
-		title: 'Sonnenschein',
-		description: 'Beschreibung kommt hier hin.'
+		title: 'Okay jetzt sind wir wieder gut',
+		description: 'Es ist aber nicht genug dass wir es wieder vergessen können. Behalt\'s aufm Schirm!'
 	},
 	{
 		src: 'photos/12.jpg',
 		alt: 'Foto 12',
-		title: 'Hand in Hand',
-		description: 'Beschreibung kommt hier hin.'
+		title: 'If only pictures could make sounds',
+		description: 'If that were the case, you would have the honour to hear Tatjana\'s contagious laughter.\n' +
+			'Next time you see her, try to make her laugh and you won\'t regret it.'
 	},
 	{
 		src: 'photos/13.jpg',
 		alt: 'Foto 13',
-		title: 'Erinnerung',
-		description: 'Beschreibung kommt hier hin.'
+		title: 'I mean do I even have to say anything?',
+		description: 'No I do not. LOOOK AT THIS MASTERPIECE (Okay the photographer was not too shabby either, but still)'
 	},
 	{
 		src: 'photos/14.jpg',
 		alt: 'Foto 14',
-		title: 'Wunderschön',
-		description: 'Beschreibung kommt hier hin.'
+		title: 'WE REMEMBERED TO FEED HER ON TIME!!!!!',
+		description: 'Wie du hier siehst, teilt Tatjana mindestens ein Merkmal mit der Rose: Sie sieht von jedem Winkel einfach HOOOT AUS'
 	},
 	{
 		src: 'photos/15.jpg',
 		alt: 'Foto 15',
-		title: 'Zusammen stark',
-		description: 'Beschreibung kommt hier hin.'
+		title: 'Und weil du es nicht vergessen hast',
+		description: 'Kriegst du auch mal ein Tolles Lächeln'
 	},
 	{
 		src: 'photos/16.jpg',
 		alt: 'Foto 16',
-		title: 'Lieblingsmensch',
-		description: 'Beschreibung kommt hier hin.'
+		title: 'AND ANOTHER ONE',
+		description: 'Die Drinks schmecken auch einfach GUUUUT'
 	},
 	{
 		src: 'photos/17.jpg',
 		alt: 'Foto 17',
-		title: 'Du und ich',
-		description: 'Beschreibung kommt hier hin.'
+		title: 'End of the Tour',
+		description: 'Please leave through the gift shop. Or swipe back and enjoy the show all over again!'
 	}
 ];
