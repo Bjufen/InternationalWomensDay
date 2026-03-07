1f77bbebc7ce19ae2d3b9ddbd98d63bbfb537fa5
Yusuf Shehadeh <y.shehadeh@knuddels.de>
2026-03-07 05:52:53 +0100

    Make changes to the letter

--- src/lib/data/letter.ts
+++ src/lib/data/letter.ts
@@ -1,11 +1,13 @@
-export const letterText = `Liebe Tatjana,
+export const letterText = `Tatjana,
 
 heute ist dein Tag – und die Welt ist ein besserer Ort, weil du in ihr bist.
 
-Du bist die Frau, die jeden Raum mit Wärme füllt, die mit einem Lächeln alles heller macht und deren Stärke mich jeden Tag aufs Neue beeindruckt.
+Du bist die Frau, die jeden Raum mit Wärme füllt, die mit einem Lächeln alles heller macht und deren Stärke dein Umfeld jeden Tag aufs Neue beeindruckt.
 
 Zum Internationalen Frauentag möchte ich dir sagen: Du bist außergewöhnlich. Nicht nur heute, sondern jeden einzelnen Tag.
 
-Diese kleine Seite ist für dich – ein digitaler Garten, ein Blick in die Sterne und ein paar Erinnerungen, die zeigen, wie besonders du bist.
+Leider kann ich heute nicht da sein um dich richtig zu feiern. Dafür habe ich eine kleine (ODER IST SIE GROß????) Überraschung für dich vorbereitet.
 
-In Liebe`;
+Bis dahin: Diese kleine Seite ist für dich – ein digitaler Garten, ein Blick in die Sterne und ein paar Erinnerungen, die zeigen, wie besonders und cool du bist!
+
+Yusuf`;
