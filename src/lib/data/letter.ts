a4cba81664aeb399ca7522a410e6dad948d3668c
Yusuf Shehadeh <y.shehadeh@knuddels.de>
2026-03-07 06:01:26 +0100

    Make changes to the letter

--- src/lib/data/letter.ts
+++ src/lib/data/letter.ts
@@ -6,7 +6,7 @@ Du bist die Frau, die jeden Raum mit Wärme füllt, die mit einem Lächeln alles
 
 Zum Internationalen Frauentag möchte ich dir sagen: Du bist außergewöhnlich. Nicht nur heute, sondern jeden einzelnen Tag.
 
-Leider kann ich heute nicht da sein um dich richtig zu feiern. Dafür habe ich eine kleine (ODER IST SIE GROß????) Überraschung für dich vorbereitet.
+Leider kann ich heute nicht da sein um dich richtig zu feiern. Dafür habe ich eine kleine Überraschung (ODER IST SIE GROß????) für dich vorbereitet.
 
 Bis dahin: Diese kleine Seite ist für dich: ein digitaler Garten, ein Blick in die Sterne und ein paar Erinnerungen, die zeigen, wie besonders und cool du bist!
 
